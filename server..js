d5562601306dd798747aad8fb7c1ca51d9da6670
satish1872 <satish18072000@gmail.com>
2020-04-10 14:42:58 +0530

    2nd commit

--- server..js
+++ server..js
@@ -19,4 +19,6 @@ app.use('/public', require('./routes/public'))
 app.use('/private', require('./routes/private'))
 app.use('/', require('./routes/root'))
 
+
+
 app.listen(9876, () => console.log("Server running on http://localhost:9876"))
